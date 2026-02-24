f41ee6895d41ef71512363087d257f658d64d2bf
cindyjag <147973833+cindyjag@users.noreply.github.com>
2026-02-24 16:16:40 +0100

    Update motor.R

--- motor.R
+++ motor.R
@@ -17,12 +17,14 @@ library(sjPlot)
 library(ggplot2)
 library(ggeffects)
 library(emmeans)
+library(performance)
+
 
 # ==============================#
 #          Data Import          #
 # ==============================#
 
-all_data <- read.table("D:/dat/motor_data.txt", header = TRUE, sep = "\t")
+all_data <- read.table("D:/motor_data.txt", header = TRUE, sep = "\t")
 
 
 # ==============================#
@@ -81,10 +83,22 @@ all_data_cleaned <- all_data %>%
 # ==============================#
 
 # --- Model for timeRatio ---
-model_timeRatio <- lmer(timeRatio ~ scondition + (1 | subj) + (1 | tcondition),
+model_timeRatio <- lmer(timeRatio ~ scondition + tcondition + (tcondition | subj) ,
                         data = all_data_cleaned)
+
+model_timeRatio_qu <- lmer(timeRatio ~ scondition  + I(scondition^2)+ tcondition + (tcondition | subj) ,
+                        data = all_data_cleaned)
+
+anova(model_timeRatio, model_timeRatio_qu)
+
+
 summary(model_timeRatio)
 tab_model(model_timeRatio)
+res_timeRatio <- simulateResiduals(
+  fittedModel = model_timeRatio,
+  n = 1000
+)
+plot(res_timeRatio)
 
 # --- Subject-level betas for timeRatio ---
 beta_int <- all_data_cleaned %>%
@@ -121,7 +135,7 @@ ggplot(summary_data, aes(x = scondition, y = mean_timeRatio, color = factor(scon
   geom_errorbar(aes(ymin = mean_timeRatio - se_timeRatio,
                     ymax = mean_timeRatio + se_timeRatio),
                 width = 0.2, size = 1) +
-  geom_abline(intercept = 1.153, slope = 0.0143, color = "#00A08A", size = 1) +
+  #geom_abline(intercept = 1.153, slope = 0.0143, color = "#00A08A", size = 1) +
   labs(x = "Spatial Interstimulus Interval", y = "Ratio of Temporal Estimate") +
   coord_cartesian(ylim = c(1.125, 1.23)) +
   theme_minimal() +
@@ -135,8 +149,21 @@ ggplot(summary_data, aes(x = scondition, y = mean_timeRatio, color = factor(scon
 
 model_xRatio <- lmer(xRatio ~ tcondition + (1 | subj) + (1 | scondition),
                      data = all_data_cleaned)
+
+model_xRatio_qu <- lmer(xRatio ~ tcondition + + I(tcondition^2) + (1 | subj) + (1 | scondition),
+                     data = all_data_cleaned)
+
+anova(model_xRatio, model_xRatio_qu)
+
+r2(model_xRatio)
 summary(model_xRatio)
 tab_model(model_xRatio)
+
+res_xRatio <- simulateResiduals(
+  fittedModel = model_xRatio,
+  n = 1000
+)
+plot(res_xRatio)
 
 # --- Subject-level betas for xRatio ---
 betas_int <- all_data_cleaned %>%
@@ -151,11 +178,76 @@ betas_int <- all_data_cleaned %>%
 # Merge perception and interception betas if available
 # (Assumes `beta_perc` and `betas_perc` are loaded from perception analysis)
  all_beta_space <- merge(beta_space, betas_int, by = c("subj", "term"))
+ all_beta_space<-all_beta_space %>%
+   filter(subj != 25)
  cor.test(all_beta_space[all_beta_space$term == "tcondition",]$estimate.x, all_beta_space[all_beta_space$term == "tcondition",]$estimate.y)
  
  all_beta_time <- merge(beta_time, beta_int, by = c("subj", "term"))
  cor.test(all_beta_time[all_beta_time$term == "scondition",]$estimate.x, all_beta_time[all_beta_time$term == "scondition",]$estimate.y)
 
+
+
+
+ ggplot(all_beta_time[all_beta_time$term == "scondition",], aes(x = estimate.x, y = estimate.y)) +
+  geom_point(size = 3, alpha = 0.85, color = "darkgrey") +
+  geom_smooth(method = "lm", se = FALSE, color = "black") +
+  labs(title = "space-on-time effect",
+    x = "individual betas perceptual task",
+    y = "individual betas motor task"
+  ) +
+  theme_minimal(base_size = 14)+
+  large_text_theme
+ 
+ 
+ ggplot(all_beta_space[all_beta_space$term == "tcondition",], aes(x = estimate.x, y = estimate.y)) +
+   geom_point(size = 3, alpha = 0.85, color = "darkgrey") +
+   geom_smooth(method = "lm", se = FALSE, color = "black") +
+   labs(title = "time-on-space effect",
+     x = "individual betas perceptual task",
+     y = "individual betas motor task"
+   ) +
+   theme_minimal(base_size = 14)+
+   large_text_theme
+ 
+
+ 
+ 
+ plot_data <- bind_rows(
+   all_beta_time %>%
+     filter(term == "scondition") %>%
+     mutate(effect = "space-on-time effect"),
+   all_beta_space %>%
+     filter(term == "tcondition") %>%
+     mutate(effect = "time-on-space effect")
+ )
+ 
+ ggplot(plot_data, aes(x = estimate.x, y = estimate.y)) +
+   geom_point(
+     size = 3,
+     alpha = 0.8,
+     color = "grey40"
+   ) +
+   geom_smooth(
+     method = "lm",
+     se = FALSE,
+     color = "black",
+     linewidth = 0.8
+   ) +
+   facet_wrap(~ effect) +
+   labs(
+     x = "linear beta estimates (perceptual task)",
+     y = "linear beta estimates (motor task)"
+   ) +
+   theme_minimal(base_size = 14) +
+   large_text_theme +
+   theme(
+     strip.text = element_text(face = "bold"),
+     panel.grid.minor = element_blank()
+   )
+ 
+ 
+ 
+ 
 # ==============================#
 #       Group Means for xRatio  #
 # ==============================#
@@ -177,7 +269,7 @@ ggplot(summary_data2, aes(x = tcondition, y = mean_spaceRatio, color = factor(tc
   geom_errorbar(aes(ymin = mean_spaceRatio - se_spaceRatio,
                     ymax = mean_spaceRatio + se_spaceRatio),
                 width = 0.2, size = 1) +
-  geom_abline(intercept = 1.208, slope = -0.0174, color = "#00A08A", size = 1) +
+  #geom_abline(intercept = 1.208, slope = -0.0174, color = "#00A08A", size = 1) +
   labs(x = "Temporal Interstimulus Interval", y = "Ratio of Spatial Estimate") +
   coord_cartesian(ylim = c(1.125, 1.23)) +
   theme_minimal() +
